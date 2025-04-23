fd79d1c29b89d169369e0570750506f06ad95572
YSH1214 <hktysh@gmail.com>
2025-04-23 17:08:15 +0900

    변경사항 반영

--- fishbread_shop.py
+++ fishbread_shop.py
@@ -12,21 +12,21 @@ sales = {
     "슈크림붕어빵" : 0,
     "초코붕어빵" : 0
 }
-
 def order_bread():
     while True:    
         bread_type = input("주문할 붕어빵을 선택하세요(팥붕어빵, 슈크림붕어빵, 초코붕어빵) 만약 뒤로가길 원하시면 뒤로가기를 입력해주세요: ")
         if bread_type == "뒤로가기":
             break
-        bread_count = int(input("주문할 개수를 입력하세요: ")) #8
-        if  stock[bread_type] >= bread_count:
-            stock[bread_type] -= bread_count
-            sales[bread_type] += bread_count
-            print(f"{bread_type} {bread_count}개가 판매되었습니다.")
+        if bread_type in stock:
+            bread_count = int(input("주문할 개수를 입력하세요: ")) #8
+            if  stock[bread_type] >= bread_count:
+                stock[bread_type] -= bread_count
+                sales[bread_type] += bread_count
+                print(f"{bread_type} {bread_count}개가 판매되었습니다.")
+            else:
+                print(f"재고가 부족합니다. 현재 {stock[bread_type]}개만 주문 가능합니다.")
         else:
-            print(f"재고가 부족합니다. 현재 {stock[bread_type]}개만 주문 가능합니다.")
-    
-
+            print("정신을 똑바로 차리시고 주문을 다시해주세요👺")
 while True:
     mode = input("원하는 모드를 선택하세요(주문, 관리자, 종료): ") #주문
     #mode = "종료"
